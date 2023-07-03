a99c3f7288013b568103eddf1a156582d232d328
iamkinglana <ngetichenock32@gmail.com>
2023-07-03 21:08:03 +0300

    likes Update

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,20 +2,30 @@ class LikesController < ApplicationController
 
     def create
       cast = Cast.find(params[:cast_id])
-      like = cast.likes.build(user: current_user)
+      like = cast.likes.find_by(user: current_user)
 
-      if like.save
-        render json: like
+      if like
+        render json: { error: 'User has already liked this cast' }, status: :unprocessable_entity
       else
-        render json: { error: like.errors.full_messages }, status: :unprocessable_entity
+        like = cast.likes.build(user: current_user)
+
+        if like.save
+          cast.update(likes: cast.likes + 1) # Increment likes count
+          render json: like
+        else
+          render json: { error: like.errors.full_messages }, status: :unprocessable_entity
+        end
       end
     end
 
     def destroy
       like = current_user.likes.find(params[:id])
+      cast = like.cast
+
       like.destroy
+      cast.update(likes: cast.likes - 1) # Decrement likes count
+
       render json: { message: 'Like removed successfully' }
     end
-  end
 
 end
