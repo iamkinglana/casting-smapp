893ad6905d2dd249db8c53f69dfce0ca72bb3e8c
iamkinglana <ngetichenock32@gmail.com>
2023-07-03 14:50:28 +0300

    Cast Update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,35 @@
 class CommentsController < ApplicationController
+     def create
+      cast = Cast.find(params[:cast_id])
+      comment = cast.comments.build(comment_params)
+      comment.user = current_user
+
+      if comment.save
+        render json: comment
+      else
+        render json: { error: comment.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      comment = current_user.comments.find(params[:id])
+      if comment.update(comment_params)
+        render json: { message: 'Comment updated successfully' }
+      else
+        render json: { error: comment.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      comment = current_user.comments.find(params[:id])
+      comment.destroy
+      render json: { message: 'Comment deleted successfully' }
+    end
+
+    private
+
+    def comment_params
+      params.permit(:content)
+    end
+
 end
--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,20 @@
 class FollowsController < ApplicationController
+
+    def create
+      followed_user = User.find(params[:followed_user_id])
+      follow = current_user.follows.build(followed_user: followed_user)
+
+      if follow.save
+        render json: follow
+      else
+        render json: { error: follow.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      follow = current_user.follows.find(params[:id])
+      follow.destroy
+      render json: { message: 'Unfollowed successfully' }
+    end
+
 end
