2575a75d38f9b1ed671f478b5861138328b37c29
iamkinglana <ngetichenock32@gmail.com>
2023-07-04 00:37:58 +0300

    User Update

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,20 +1,28 @@
 class FollowsController < ApplicationController
+  def create
+    followed_user = User.find(params[:followed_user_id])
+    follow = current_user.follows.build(followed_user: followed_user)
 
-    def create
-      followed_user = User.find(params[:followed_user_id])
-      follow = current_user.follows.build(followed_user: followed_user)
-
-      if follow.save
-        render json: follow
-      else
-        render json: { error: follow.errors.full_messages }, status: :unprocessable_entity
-      end
+    if follow.save
+      update_follower_counts(followed_user)
+      render json: follow
+    else
+      render json: { error: follow.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def destroy
-      follow = current_user.follows.find(params[:id])
-      follow.destroy
-      render json: { message: 'Unfollowed successfully' }
-    end
+  def destroy
+    follow = current_user.follows.find(params[:id])
+    followed_user = follow.followed_user
+    follow.destroy
+    update_follower_counts(followed_user)
+    render json: { message: 'Unfollowed successfully' }
+  end
 
+  private
+
+  def update_follower_counts(user)
+    user.update(followers_count: user.followers.count)
+    current_user.update(following_count: current_user.following.count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_many :casts
   has_many :comments
   has_many :follows
+
+  has_many :follower_relationships, foreign_key: :followed_user_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :followed_user_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :followed_user_relationships, source: :followed_user
+
 end
